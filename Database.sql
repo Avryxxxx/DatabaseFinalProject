00859cb23d1877fd66c905ebef121a44a685d0b4
Avryxxxx <nesnesjalandoni@gmail.com>
2023-12-09 20:42:52 +0800

    feat create student tables syntax

--- Database.sql
+++ Database.sql
@@ -5,3 +5,15 @@ CREATE TABLE Users (
     Username VARCHAR(50) NOT NULL,
     Password VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE StudentInfo (
+  StdId INT PRIMARY KEY,
+  StdFirstName VARCHAR(15),
+  StdLastName VARCHAR(16),
+  Birthdate DATE,
+  Gender VARCHAR(10),
+  Phone VARCHAR(15),
+  Address TEXT,
+  Photo LONGBLOB
+);
+
